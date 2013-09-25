4f0f17b99b7115f98a6e64845ac34ddc1fd06a92
Juan Garcia <Juan Garcia@JuanGarcia.unisabana.edu.co>
2013-09-25 08:19:36 -0500

    subiendo repositorio Juan

---
+++ Ejemplo1/src/MainJuan/MainJuan.java
@@ -0,0 +1,20 @@
+import javax.swing.JOptionPane;
+
+
+public class MainJuan {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+
+		
+		JOptionPane.showMessageDialog(null, "Mega Pelos");
+		
+		
+		
+	}
+
+}
